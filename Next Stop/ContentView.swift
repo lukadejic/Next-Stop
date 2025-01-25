b776a3f15107d9d132c887ba829dba011c294e19
lukadejic <lukadejic111@gmail.com>
2025-01-25 16:12:07 +0100

    Initial Commit

---
+++ Next Stop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Next Stop
+//
+//  Created by MacBook on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
