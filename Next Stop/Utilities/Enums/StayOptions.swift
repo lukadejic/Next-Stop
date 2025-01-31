db180eb66eae6d7207d0cea656b191a80df3c6ec
lukadejic <lukadejic111@gmail.com>
2025-01-31 11:50:36 +0100

    Added stayOptionEnum

--- Next Stop/Utilities/Enums/StayOptions.swift
+++ Next Stop/Utilities/Enums/StayOptions.swift
@@ -1,8 +1,20 @@
-//
-//  StayOptions.swift
-//  Next Stop
-//
-//  Created by MacBook on 1/30/25.
-//
 
 import Foundation
+
+enum StayOptionType: String, CaseIterable {
+    case rooms = "Rooms"
+    case beachfront = "Beachfront"
+    case camping = "Camping"
+    case food = "Food"
+    case explore = "Explore"
+    
+    var imageName: String {
+        switch self {
+        case .rooms: return "house.fill"
+        case .beachfront: return "beach.umbrella"
+        case .camping: return "tent.2"
+        case .food: return "fork.knife"
+        case .explore: return "map.fill"
+        }
+    }
+}
---
+++ Next Stop/Views/Home/HomeView.swift
@@ -0,0 +1,69 @@
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var selectedStayOption: StayOptionType = .rooms
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                SearchBarView()
+
+                stayOptionsHeader
+                Spacer()
+            }
+        }
+    }
+
+}
+
+private extension HomeView {
+    var stayOptionsHeader: some View {
+        ScrollView(.horizontal){
+            HStack {
+                ForEach(StayOptionType.allCases, id: \.self) { option in
+                    stayOptionView(for: option)
+                }
+            }
+        }
+    }
+    
+    func stayOptionView(for option: StayOptionType) -> some View {
+        VStack {
+            Image(systemName: option.imageName)
+                .fontWeight(.light)
+                .foregroundStyle(.black.opacity(0.8))
+                .frame(width: 30, height: 40)
+                .font(.title)
+                .padding(.bottom, 5)
+                .padding(.horizontal, 20)
+            
+            Text(option.rawValue)
+                .font(.subheadline)
+                .fontWeight(.medium)
+            
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 50, height: 3)
+                .padding(.top, 5)
+                .opacity(selectedStayOption == option ? 0.8 : 0)
+        }
+        .scaleEffect(
+            withAnimation(.easeOut, {
+                selectedStayOption == option ? 1.2 : 1
+            })
+            
+        )
+        .opacity(selectedStayOption == option ? 1 : 0.5)
+        .onTapGesture {
+            withAnimation {
+                selectedStayOption = option
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
