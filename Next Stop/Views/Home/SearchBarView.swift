967d31f032ed9faa58d24f3b8c30181e470e0ffd
lukadejic <lukadejic111@gmail.com>
2025-01-31 11:57:52 +0100

    Added a custom search bar button

--- Next Stop/Views/Home/SearchBarView.swift
+++ Next Stop/Views/Home/SearchBarView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    var action : (() -> ())?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading,spacing: 5){
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any Week - Add Guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button{
+                action?()
+            }label: {
+                Image(systemName:"line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical,10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.2)
+                .shadow(color: .black.opacity(0.4), radius: 10)
+        }
+        .padding()
     }
 }
 
