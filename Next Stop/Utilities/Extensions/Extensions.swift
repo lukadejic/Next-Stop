8f0ed697bd3ca41fe78c7afbdab214105ec37c1f
lukadejic <lukadejic111@gmail.com>
2025-02-26 18:48:17 +0100

    Added a calendar helpers function

--- Next Stop/Utilities/Extensions/Extensions.swift
+++ Next Stop/Utilities/Extensions/Extensions.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SwiftUI
 
-class ColorSettins {
+class CalendarColorSettins {
     var textColor = Color.primary
     var todayColor = Color.red
     var selectedColor = Color.white
@@ -19,7 +19,7 @@ class ColorSettins {
     var monthBackColor = Color.clear
 }
 
-class FontSettings {
+class CalendarFontSettings {
     var monthHeaderFont : Font = .body.bold()
     var weekdayHeaderFont : Font = .caption
     var cellUnselectedFont : Font = .body
@@ -28,3 +28,79 @@ class FontSettings {
     var cellTodayFont : Font = .body.bold()
     var cellBetweenStartAndEndFont : Font = .body.bold()
 }
+
+class CalendarHelpers {
+    
+    static func getWeekDayHeaders(calendar: Calendar) -> [String] {
+        let weekdays = calendar.shortStandaloneWeekdaySymbols
+        let firstWeekDayIndex = calendar.firstWeekday - 1
+        let adjustedWeekDays = Array(weekdays[firstWeekDayIndex...] + weekdays[..<firstWeekDayIndex])
+        return adjustedWeekDays
+    }
+    
+    static func formatDate(date: Date) -> String {
+        return date.formatted(.dateTime.day())
+    }
+    
+    static func stringFromDate(date: Date) -> String {
+        return date.formatted()
+    }
+    
+    static func getMonthDayFromDate(date: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.month], from: date)
+        
+        guard let month = components.month else {
+            fatalError("Can't get a month from a calendar")
+        }
+        
+        return month - 1
+    }
+    
+    static func getMonthHeader(date: Date) -> String {
+        let dateStyle = Date.FormatStyle()
+            .year(.defaultDigits)
+            .month(.wide)
+        return date.formatted(dateStyle)
+    }
+    
+    static func getNumberOfMonths(minDate: Date, maxDate: Date) -> Int {
+        let components = Calendar.current.dateComponents([.month], from: minDate, to: maxDate)
+        
+        guard let month = components.month else {
+            fatalError("Can't get a month from a calendar")
+        }
+        
+        return month + 1
+    }
+    
+    static func getLastDayOfMonth(date: Date, calendar: Calendar = .current) -> Date {
+        var components = calendar.dateComponents([.year, .month], from: date)
+        components.setValue(1, for: .day)
+        
+        guard let startOfMonth = calendar.date(from: components) else {
+            fatalError("Can't get a start of month from the calendar")
+        }
+        
+        return calendar.date(byAdding: .month, value: 1, to: startOfMonth)?.addingTimeInterval(-86500) ?? startOfMonth
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius : CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+    
+    
+}
+
+let daysPerWeek = 7
+var CalendarCellWidth: CGFloat = 32
+
+
