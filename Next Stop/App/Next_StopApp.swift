d54bb2b578cd180d2655e1e8cebbdf694cc39375
lukadejic <lukadejic111@gmail.com>
2025-03-16 19:49:07 +0100

    Firebase configuration

--- Next Stop/App/Next_StopApp.swift
+++ Next Stop/App/Next_StopApp.swift
@@ -1,9 +1,13 @@
-
 import SwiftUI
+import Firebase
 
 @main
 struct Next_StopApp: App {
     @StateObject private var homeVM = HomeViewModel(hotelsService: HotelsService())
+    
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
