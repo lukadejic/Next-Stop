6fc8aca2e5aac645b97ba02bcb9d3ec7293802d1
lukadejic <lukadejic111@gmail.com>
2025-03-30 11:36:33 +0200

    Added a decoder and encoder to DBUser

--- Next Stop/Models/DBUser.swift
+++ Next Stop/Models/DBUser.swift
@@ -6,6 +6,7 @@ struct DBUser : Codable {
     let photoURL: String?
     let displayName: String?
     let dateCreated : Date?
+    var biogrpahy: String?
     
     init(userId: String, email: String?, photoURL: String?, displayName: String?, dateCreated: Date?) {
         self.userId = userId
@@ -22,5 +23,34 @@ struct DBUser : Codable {
         self.displayName = user.displayName
         self.dateCreated = Date()
     }
-
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case email = "email"
+        case photoURL = "photo_url"
+        case displayName = "display_name"
+        case dateCreated = "date_created"
+        case biography = "biography"
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.userId, forKey: .userId)
+        try container.encodeIfPresent(self.email, forKey: .email)
+        try container.encodeIfPresent(self.photoURL, forKey: .photoURL)
+        try container.encodeIfPresent(self.displayName, forKey: .displayName)
+        try container.encodeIfPresent(self.dateCreated, forKey: .dateCreated)
+        try container.encodeIfPresent(self.biogrpahy, forKey: .biography)
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.userId = try container.decode(String.self, forKey: .userId)
+        self.email = try container.decodeIfPresent(String.self, forKey: .email)
+        self.photoURL = try container.decodeIfPresent(String.self, forKey: .photoURL)
+        self.displayName = try container.decodeIfPresent(String.self, forKey: .displayName)
+        self.dateCreated = try container.decodeIfPresent(Date.self, forKey: .dateCreated)
+        self.biogrpahy = try container.decodeIfPresent(String.self, forKey: .biography)
+    }
+    
 }
--- Next Stop/Utilities/Managers/UserManager.swift
+++ Next Stop/Utilities/Managers/UserManager.swift
@@ -15,31 +15,18 @@ final class UserManager : UserManagerProtocol {
         userCollection.document(userId)
     }
     
-    private let encoder: Firestore.Encoder = {
-        let encoder = Firestore.Encoder()
-        encoder.keyEncodingStrategy = .convertToSnakeCase
-        return encoder
-    }()
-    
-    private let decoder: Firestore.Decoder = {
-        let decoder = Firestore.Decoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        return decoder
-    }()
-    
     func createNewUser(user: DBUser) async throws {
         try userDocument(userId: user.userId).setData(from: user,
-                                                      merge: false,
-                                                      encoder: encoder)
+                                                      merge: false)
     }
     
     func getUser(userId: String) async throws -> DBUser {
-        try await userDocument(userId: userId).getDocument(as: DBUser.self, decoder: decoder)
+        try await userDocument(userId: userId).getDocument(as: DBUser.self)
     }
     
     func updateUserBiography(userId: String, biography: String) async throws {
         let data: [String : Any] = [
-            "biography" : biography
+            DBUser.CodingKeys.biography.rawValue : biography
         ]
         
         try await userDocument(userId: userId).updateData(data)
